4b0982342d3ab5c15d558638535187526397603e
acuyuch-2017403 <acuyuch-2017403@kinal.edu.gt>
2022-05-21 18:41:09 -0600

    Se termino el crud de registrar usuario

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/models/user.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  user:UserModel; 
+
+
+
+
+  constructor() { 
+    this.user = new UserModel('','','','','','','','');
+  }
+
+  ngOnInit(): void {
+  }
+
+  /*Importando 
+  declarandolo
+  y usarlo en html */
+
+  login(){
+    console.log(this.user);
+  }
+
+}
